a7b8a79a4b6764c229b7449119d4f1c27b365988
John Gorski <john.gorski.jr@gmail.com>
2014-12-20 20:13:01 -0800

    Add comments to functions; Mirror makeVector function in makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        cached.inverse <- NULL
+        set <- function(new.x) {
+                cached.inverse <- NULL
+                x <- new.x
+        }
+        get <- function() x
+        set.inverse <- function(inverse) cached.inverse <<- inverse
+        get.inverse <- function() cached.inverse
 }
 
 
-## Write a short comment describing this function
-
+# cacheSolve computes the inverse of the special "matrix" returned by 
+# makeCacheMatrix above.
+#
+# If the inverse has already been calculated (and the matrix has not changed),
+# then cacheSolve should retrieve the inverse from the cache.
+# This function assumes that the input matrix is always invertible.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        solve(x)
 }
